64fa1990f9ac77d17e1508f09eda33ff85785954
atkinsjeff <jwa5cz@virginia.edu>
2021-10-07 14:39:46 -0400

    adding google forms

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -13,7 +13,7 @@ shinyUI(pageWithSidebar(
 
   headerPanel("R packages for Forestry Analysis"),
   sidebarPanel(
-    selectizeInput('var1', 'Select Application', choices = c("choose" = "", c("Inventory/Mensuration", "Community Analysis", "Dendrochronology", "Modelling/Simulation",
+    selectizeInput('var1', 'Select Application', choices = c("choose" = "", c("Community Analysis", "Dendrochronology", "Inventory/Mensuration",  "Modelling/Simulation",
                                                                               "Phenology","Remote Sensing")), multiple = FALSE, selected = "Remote Sensing" ),
     # # adding the new div tag to the sidebar
     tags$div(class="header", checked=NA,
@@ -23,9 +23,12 @@ shinyUI(pageWithSidebar(
              tags$a(href="https://github.com/atkinsjeff/ForestAnalysisInR/blob/main/inst/extdata/doc/forest_modelling.pdf", 'Forest Modelling,'),
              tags$a(href="https://github.com/atkinsjeff/ForestAnalysisInR/blob/main/inst/extdata/doc/remote_sensing.pdf", "Remote Sensing,"),
              tags$a(href="https://github.com/atkinsjeff/ForestAnalysisInR/blob/main/inst/extdata/doc/phenology.pdf", "Phenology")
-    )
-  ),
 
+    ),
+    br(),
+    HTML("<p>To submit an R package to this database, please use our
+         <a href='https://forms.gle/4E89hAMUF9vzcKbR6'>ForestAnalysis in R Google Form</a></p>"),
+  ),
   mainPanel(
     DT::dataTableOutput("table")
     )
